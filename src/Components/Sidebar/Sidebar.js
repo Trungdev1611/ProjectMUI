c6a880c2f206d0f1e2d59558aaf9ae95a0459fa5
Trungdev <trungdev1611@gmail.com>
2022-09-30 20:29:30 +0700

    sidebar night 30.09

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import Drawer from '@mui/material/Drawer';
 import { ListItemIcon, ListItemText, MenuItem, MenuList } from '@mui/material';
-<<<<<<< HEAD
-// import AccessibleIcon from '@mui/icons-material/Accessible';
-=======
->>>>>>> 798f741f5fb2be82808cccfcad4d00f9880c8c8b
 import AdUnitsIcon from '@mui/icons-material/AdUnits';
 import { dataDrawer } from './const';
 const Sidebar = () => {
@@ -24,30 +20,6 @@ const Sidebar = () => {
         return arr1
     }
 
-<<<<<<< HEAD
-    const handleDrawerToggle = () => {
-    };
-    const ListDrawer = (arr, lv, arr1 = []) => {
-        if (Array.isArray(arr) && arr.length > 0) {
-            arr.forEach((item, index) => {
-                arr1.push(item.name && <MenuItem key={Math.random()} sx={{ paddingLeft: `calc(${lv} * 20px)` }}>
-                    {item.icon && <ListItemIcon>
-                        <AdUnitsIcon sx={{ color: 'rgba(238, 238, 238, 0.4)' }} />
-                    </ListItemIcon>}
-                    <ListItemText sx={{ '& .MuiTypography-root': { fontSize: '14px', color: 'rgba(238, 238, 238, 0.4)' } }} >{item.name}</ListItemText>
-                </MenuItem>)
-                if (item.chilren && item.chilren.length > 0) {
-                    ListDrawer(item.chilren, lv + 1, arr1)
-                    console.log(index)
-                }
-            })
-            console.log(arr1)
-        }
-        return arr1
-
-    }
-=======
->>>>>>> 798f741f5fb2be82808cccfcad4d00f9880c8c8b
     return (
         <Drawer
             variant="permanent"
@@ -60,23 +32,12 @@ const Sidebar = () => {
                     position: 'absolute',
                     top: '70px',
                     color: 'white',
-<<<<<<< HEAD
-                    paddingLeft: '15px'
-                },
-            }}
-        >
-            <MenuList>
-
-                {ListDrawer(dataDrawer, 0)}
-
-=======
 
                 },
             }}
         >
             <MenuList >
                 {renderMenuList(dataDrawer, 0, 20)}
->>>>>>> 798f741f5fb2be82808cccfcad4d00f9880c8c8b
             </MenuList>
         </Drawer>
 
