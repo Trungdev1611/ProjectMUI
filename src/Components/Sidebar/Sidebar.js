6777a5664bb0b81ae7e9091c74cf404ad24c5f33
Trungdev <trungdev1611@gmail.com>
2022-09-30 20:23:50 +0700

    sidebar night 30.09

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import Drawer from '@mui/material/Drawer';
-import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
-import AccessibleIcon from '@mui/icons-material/Accessible';
+import { ListItemIcon, ListItemText, MenuItem, MenuList } from '@mui/material';
+// import AccessibleIcon from '@mui/icons-material/Accessible';
 import AdUnitsIcon from '@mui/icons-material/AdUnits';
 import { dataDrawer } from './const';
 const Sidebar = () => {
 
     const handleDrawerToggle = () => {
     };
+    const ListDrawer = (arr, lv, arr1 = []) => {
+        if (Array.isArray(arr) && arr.length > 0) {
+            arr.forEach((item, index) => {
+                arr1.push(item.name && <MenuItem key={Math.random()} sx={{ paddingLeft: `calc(${lv} * 20px)` }}>
+                    {item.icon && <ListItemIcon>
+                        <AdUnitsIcon sx={{ color: 'rgba(238, 238, 238, 0.4)' }} />
+                    </ListItemIcon>}
+                    <ListItemText sx={{ '& .MuiTypography-root': { fontSize: '14px', color: 'rgba(238, 238, 238, 0.4)' } }} >{item.name}</ListItemText>
+                </MenuItem>)
+                if (item.chilren && item.chilren.length > 0) {
+                    ListDrawer(item.chilren, lv + 1, arr1)
+                    console.log(index)
+                }
+            })
+            console.log(arr1)
+        }
+        return arr1
+
+    }
     return (
         <Drawer
             variant="permanent"
@@ -20,22 +39,16 @@ const Sidebar = () => {
                     width: 250
                     , position: 'absolute',
                     top: '70px',
-                    color: 'white'
+                    color: 'white',
+                    paddingLeft: '15px'
                 },
             }}
         >
-            <List>
-                {dataDrawer.map((item, index) => (
-                    <ListItem key={Math.random()} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon sx={{ color: 'rgb(238, 238, 238)' }}>
-                                {index % 2 === 0 ? <AccessibleIcon /> : <AdUnitsIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={item.parent.name} sx={{ '& .MuiTypography-root': { fontSize: '14px', color: 'rgb(238, 238, 238)' } }} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
+            <MenuList>
+
+                {ListDrawer(dataDrawer, 0)}
+
+            </MenuList>
         </Drawer>
 
     )
--- src/Components/Sidebar/const.js
+++ src/Components/Sidebar/const.js
@@ -1,144 +1,157 @@
 export const dataDrawer = [
     {
-        parent: {
-            name: 'PAGES',
-            chilren: [
-                {
-                    id: 1,
-                    label: 'Dashboard',
-                    icon: '',
-                    route: 'Dashboard',
-                    component: '',
-                    iconRight: ''
-                },
-                {
-                    id: 2,
-                    label: 'Dashboard',
-                    iconleft: '',
-                    route: 'Dashboard',
-                    component: '',
-                    iconRight: ''
-                },
-                {
-                    id: 3,
-                    label: 'Dashboard',
-                    iconleft: '',
-                    route: 'Dashboard',
-                    component: '',
-                    iconRight: ''
-                },
-                {
-                    id: 4,
-                    label: 'Dashboard',
-                    iconleft: '',
-                    route: 'Dashboard',
-                    component: '',
-                    iconRight: ''
-                },
-                {
-                    id: 5,
-                    label: 'Dashboard',
-                    iconleft: '',
-                    route: 'Dashboard',
-                    component: '',
-                    iconRight: ''
 
-                }]
-        }
+        name: 'PAGES',
+        chilren: [
+            {
+                id: 1,
+                name: 'Dashboard4',
+                iconleft: '',
+                route: 'Dashboard',
+                component: '',
+                iconRight: ''
+            },
+            {
+                id: 2,
+                name: 'Dashboard5',
+                iconleft: '',
+                route: 'Dashboard',
+                component: '',
+                iconRight: ''
+            },
+            {
+                id: 2,
+                name: 'Dashboard5',
+                iconleft: '',
+                route: 'Dashboard',
+                component: '',
+                iconRight: ''
+            },
+            {
+                chilren: [
+                    {
+                        id: 7,
+                        name: 'Dashboard7',
+                        iconleft: '',
+                        route: 'Dashboard',
+                        component: '',
+                        iconRight: ''
+                    },
+                    {
+                        id: 4,
+                        name: 'Dashboard',
+                        iconleft: '',
+                        route: 'Dashboard',
+                        component: '',
+                        iconRight: ''
+                    },
+                    {
+                        id: 5,
+                        name: 'Dashboard',
+                        iconleft: '',
+                        route: 'Dashboard',
+                        component: '',
+                        iconRight: ''
+                    }
+                ]
+            }
+        ]
+
+
     },
     {
-        parent: {
-            name: 'ELEMENTS',
-            chilren: [
-                {
-                    id: 1,
-                    label: 'Dashboard',
-                    icon: '',
-                    route: 'Dashboard',
-                    component: '',
-                    iconRight: ''
-                },
-                {
-                    id: 2,
-                    label: 'Dashboard',
-                    iconleft: '',
-                    route: 'Dashboard',
-                    component: '',
-                    iconRight: ''
-                },
-                {
-                    id: 3,
-                    label: 'Dashboard',
-                    iconleft: '',
-                    route: 'Dashboard',
-                    component: '',
-                    iconRight: ''
-                },
-                {
-                    id: 4,
-                    label: 'Dashboard',
-                    iconleft: '',
-                    route: 'Dashboard',
-                    component: '',
-                    iconRight: ''
-                },
-                {
-                    id: 5,
-                    label: 'Dashboard',
-                    iconleft: '',
-                    route: 'Dashboard',
-                    component: '',
-                    iconRight: ''
 
-                }]
-        }
+        name: 'ELEMENTS',
+        chilren: [
+            {
+                id: 1,
+                name: 'Dashboard1',
+                iconleft: '',
+                route: 'Dashboard',
+                component: '',
+                iconRight: ''
+            },
+            {
+                id: 2,
+                name: 'Dashboard2',
+                iconleft: '',
+                route: 'Dashboard',
+                component: '',
+                iconRight: ''
+            },
+            {
+                id: 3,
+                name: 'Dashboard3',
+                iconleft: '',
+                route: 'Dashboard',
+                component: '',
+                iconRight: ''
+            },
+            {
+                id: 4,
+                name: 'Dashboard',
+                iconleft: '',
+                route: 'Dashboard',
+                component: '',
+                iconRight: ''
+            },
+            {
+                id: 5,
+                name: 'Dashboard',
+                iconleft: '',
+                route: 'Dashboard',
+                component: '',
+                iconRight: ''
+
+            }]
+
     },
     {
-        parent: {
-            name: 'MIRA',
-            chilren: [
-                {
-                    id: 1,
-                    label: 'Dashboard',
-                    icon: '',
-                    route: 'Dashboard',
-                    component: '',
-                    iconRight: ''
-                },
-                {
-                    id: 2,
-                    label: 'Dashboard',
-                    iconleft: '',
-                    route: 'Dashboard',
-                    component: '',
-                    iconRight: ''
-                },
-                {
-                    id: 3,
-                    label: 'Dashboard',
-                    iconleft: '',
-                    route: 'Dashboard',
-                    component: '',
-                    iconRight: ''
-                },
-                {
-                    id: 4,
-                    label: 'Dashboard',
-                    iconleft: '',
-                    route: 'Dashboard',
-                    component: '',
-                    iconRight: ''
-                },
-                {
-                    id: 5,
-                    label: 'Dashboard',
-                    iconleft: '',
-                    route: 'Dashboard',
-                    component: '',
-                    iconRight: ''
 
-                }]
-        }
-    },
+        name: 'MIRA',
+        chilren: [
+            {
+                id: 1,
+                name: 'Dashboard',
+                iconleft: '',
+                route: 'Dashboard',
+                component: '',
+                iconRight: ''
+            },
+            {
+                id: 2,
+                name: 'Dashboard',
+                iconleft: '',
+                route: 'Dashboard',
+                component: '',
+                iconRight: ''
+            },
+            {
+                id: 3,
+                name: 'Dashboard',
+                iconleft: '',
+                route: 'Dashboard',
+                component: '',
+                iconRight: ''
+            },
+            {
+                id: 4,
+                name: 'Dashboard',
+                iconleft: '',
+                route: 'Dashboard',
+                component: '',
+                iconRight: ''
+            },
+            {
+                id: 5,
+                name: 'Dashboard',
+                iconleft: '',
+                route: 'Dashboard',
+                component: '',
+                iconRight: ''
+
+            }]
+
+    }
 
 ]
